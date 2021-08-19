0bb626337dc8e63ac9db42bf2fc87700c07ea0aa
sonchaegeon <thscorjs1002@naver.com>
2021-08-19 19:38:28 +0900

    📑 :: (#11) code_area 엔티티

---
+++ src/main/java/com/github/football/entity/code/Area.java
@@ -0,0 +1,29 @@
+package com.github.football.entity.code;
+
+import com.github.football.entity.club.Club;
+import com.github.football.entity.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "code_area")
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "area")
+    private List<User> users = new LinkedList<>();
+
+    @OneToMany(mappedBy = "area")
+    private List<Club> clubs = new LinkedList<>();
+}
