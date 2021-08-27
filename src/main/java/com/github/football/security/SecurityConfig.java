6843d013d9aadebe5dc5eaa82cbef9e5d7086739
sonchaegeon <thscorjs1002@naver.com>
2021-08-27 22:33:47 +0900

    📑 :: (#21) security 설정
    /users/auth/google permit all

---
+++ src/main/java/com/github/football/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.github.football.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/users/auth/google").permitAll()
+                .anyRequest().authenticated();
+    }
+}
