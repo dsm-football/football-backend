35a20b85f72fac1fd8a2b9b7d0bf31dfc5ec25ce
sonchaegeon <thscorjs1002@naver.com>
2021-08-29 16:18:06 +0900

    📑 :: (#23) 구글 유저 정보 api response

---
+++ src/main/java/com/github/football/util/api/dto/google/response/GoogleInfoResponse.java
@@ -0,0 +1,11 @@
+package com.github.football.util.api.dto.google.response;
+
+import lombok.Getter;
+
+@Getter
+public class GoogleInfoResponse {
+
+    private String picture;
+    private String email;
+    private String name;
+}
