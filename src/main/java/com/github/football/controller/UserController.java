af5a3c58b63397ffbc7618bb9ad9b2c85ad4f438
sonchaegeon <thscorjs1002@naver.com>
2021-08-29 16:16:47 +0900

    ⚡️ :: (#23) 구글 토큰 가져오기 controller

--- src/main/java/com/github/football/controller/UserController.java
+++ src/main/java/com/github/football/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.github.football.controller;
 
 import com.github.football.dto.user.response.LinkResponse;
+import com.github.football.dto.user.response.TokenResponse;
 import com.github.football.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
@@ -16,8 +15,13 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/auth/google")
+    @GetMapping("auth/google")
     public ResponseEntity<LinkResponse> getGoogleLink() {
         return new ResponseEntity<>(userService.getGoogleLink(), HttpStatus.OK);
+    }
+
+    @PostMapping("auth/google")
+    public ResponseEntity<TokenResponse> getGoogleTokenByCode(@RequestParam("code") String code) {
+        return new ResponseEntity<>(userService.getGoogleTokenByCode(code), HttpStatus.OK);
     }
 }
