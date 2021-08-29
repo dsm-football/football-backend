80ab6ac21314e4ea7879af07897c7e5227d1c56e
sonchaegeon <thscorjs1002@naver.com>
2021-08-29 16:25:03 +0900

    ⚡️ :: (#23) findByEmail method

---
+++ src/main/java/com/github/football/entity/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.github.football.entity.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
