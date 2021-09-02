338795d8f3fbfad705d6146d7b308258a879b2ec
sonchaegeon <thscorjs1002@naver.com>
2021-09-02 23:37:54 +0900

    ⚡️ :: (#25) error response

---
+++ src/main/java/com/github/football/exception/error/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.github.football.exception.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private final int status;
+    private final String message;
+
+    @Override
+    public String toString() {
+        return "{\n" +
+                "\t\"status\": " + status +
+                ",\n\t\"message\": \"" + message + '\"' +
+                "\n}";
+    }
+}
