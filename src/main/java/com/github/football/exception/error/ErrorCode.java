6de93e25c3716e6f6b68e0d431e8a3f4cd09afaa
sonchaegeon <thscorjs1002@naver.com>
2021-09-02 23:37:37 +0900

    ⚡️ :: (#25) error code
    user not found

---
+++ src/main/java/com/github/football/exception/error/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.github.football.exception.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    USER_NOT_FOUND(404, "User not found");
+
+    private final int status;
+    private final String message;
+}
