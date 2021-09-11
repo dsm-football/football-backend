bd63d4869cccdc43576287827d4d3aa4ec3d0f0d
sonchaegeon <thscorjs1002@naver.com>
2021-09-11 14:41:26 +0900

    🧪 :: (#30) 테스트 환경 점검 테스트

--- src/test/java/com/github/football/FootballApplicationTests.java
+++ src/test/java/com/github/football/FootballApplicationTests.java
@@ -1,8 +1,17 @@
 package com.github.football;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FootballApplicationTests {
+
+    @Test
+    @DisplayName("테스트 환경 점검 테스트")
+    void test() {
+
+    }
 }
