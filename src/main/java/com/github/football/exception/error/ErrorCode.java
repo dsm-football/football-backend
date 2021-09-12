0eabd2bf97c7bb033144fc8c7729e8c5b5756b44
sonchaegeon <thscorjs1002@naver.com>
2021-09-12 13:59:09 +0900

    ⚡️ :: (#29) error codes
    invalid input value, expired access token, credentials not found, user not found, area not found, cycle not found, gender not found

--- src/main/java/com/github/football/exception/error/ErrorCode.java
+++ src/main/java/com/github/football/exception/error/ErrorCode.java
@@ -9,7 +9,13 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
-    USER_NOT_FOUND(404, "User not found");
+    INVALID_INPUT_VALUE(400, "Invalid input value."),
+    EXPIRED_ACCESS_TOKEN(401, "Expired access token"),
+    CREDENTIALS_NOT_FOUND(401, "Credentials not found"),
+    USER_NOT_FOUND(404, "User not found"),
+    AREA_NOT_FOUND(404, "Area not found"),
+    CYCLE_NOT_FOUND(404, "Cycle not found"),
+    GENDER_NOT_FOUND(404, "Gender not found");
 
     private final int status;
     private final String message;
