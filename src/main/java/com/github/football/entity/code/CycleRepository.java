55c454259d6115870cd391ff5ac4467c3fdb4cfe
sonchaegeon <thscorjs1002@naver.com>
2021-09-12 14:00:28 +0900

    ⚡️ :: (#29) cycle repository

---
+++ src/main/java/com/github/football/entity/code/CycleRepository.java
@@ -0,0 +1,6 @@
+package com.github.football.entity.code;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CycleRepository extends CrudRepository<Cycle, Integer> {
+}
