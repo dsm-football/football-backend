05c3e18cef0ce5f5192fc0ed28252821c49f6a33
sonchaegeon <thscorjs1002@naver.com>
2021-09-12 13:51:07 +0900

    ⚡️ :: (#29) post club service 추상화

---
+++ src/main/java/com/github/football/service/club/ClubService.java
@@ -0,0 +1,8 @@
+package com.github.football.service.club;
+
+import com.github.football.dto.feed.request.PostClubRequest;
+
+public interface ClubService {
+
+    void postClub(PostClubRequest request);
+}
