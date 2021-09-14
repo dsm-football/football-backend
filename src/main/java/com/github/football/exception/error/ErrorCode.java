ed96697c16bd86c656b1e0cba2872bd0741ac9e6
sonchaegeon <thscorjs1002@naver.com>
2021-09-14 13:15:52 +0900

    ⚡️ :: (#29) error codes
    already used name exception, already joined club exception, age group not found exception

--- src/main/java/com/github/football/exception/error/ErrorCode.java
+++ src/main/java/com/github/football/exception/error/ErrorCode.java
@@ -9,13 +9,16 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
-    INVALID_INPUT_VALUE(400, "Invalid input value."),
+    INVALID_INPUT_VALUE(400, "Invalid input value"),
+    ALREADY_JOINED_CLUB(400, "Already joined club"),
+    ALREADY_USED_NAME(400, "Already used name"),
     EXPIRED_ACCESS_TOKEN(401, "Expired access token"),
     CREDENTIALS_NOT_FOUND(401, "Credentials not found"),
     USER_NOT_FOUND(404, "User not found"),
     AREA_NOT_FOUND(404, "Area not found"),
     CYCLE_NOT_FOUND(404, "Cycle not found"),
-    GENDER_NOT_FOUND(404, "Gender not found");
+    GENDER_NOT_FOUND(404, "Gender not found"),
+    AGE_GROUP_NOT_FOUND(404, "Age group not found");
 
     private final int status;
     private final String message;
