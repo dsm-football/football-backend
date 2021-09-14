a0158488b3deec90e73cc0c6db7639680ed18b7f
sonchaegeon <thscorjs1002@naver.com>
2021-09-14 13:14:41 +0900

    ⚡️ :: (#29) 가독성

--- src/main/java/com/github/football/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/github/football/security/jwt/JwtTokenProvider.java
@@ -61,8 +61,8 @@ public class JwtTokenProvider {
 
     public String resolveToken(HttpServletRequest request) {
         String bearer = request.getHeader(header);
-        if(bearer != null && bearer.length() > 7 && bearer.startsWith(prefix)) {
-            return bearer.substring(7);
+        if(bearer != null && bearer.startsWith(prefix)) {
+            return bearer.replace(prefix, "");
         }
         return null;
     }
