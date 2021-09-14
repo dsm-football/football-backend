f8ebce5dc582fe936cf982f5163b3dec051a17db
sonchaegeon <thscorjs1002@naver.com>
2021-09-14 18:23:37 +0900

    🐛 :: (#29) allowed origin swagger

--- src/main/java/com/github/football/config/WebConfig.java
+++ src/main/java/com/github/football/config/WebConfig.java
@@ -8,6 +8,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "dsm-football.github.io");
+                .allowedOrigins("http://localhost:3000", "https://dsm-football.github.io");
     }
 }
