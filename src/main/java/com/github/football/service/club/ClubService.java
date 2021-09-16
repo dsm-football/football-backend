bfeac4631a11333d65ddf003b605910e7837b01d
sonchaegeon <thscorjs1002@naver.com>
2021-09-16 22:47:25 +0900

    ⚡️ :: (#31) post club applicant 추상화

--- src/main/java/com/github/football/service/club/ClubService.java
+++ src/main/java/com/github/football/service/club/ClubService.java
@@ -1,8 +1,11 @@
 package com.github.football.service.club;
 
-import com.github.football.dto.feed.request.PostClubRequest;
+import com.github.football.dto.club.request.PostClubRequest;
+import com.github.football.dto.club.request.ToggleApplicantRequest;
+import com.github.football.dto.club.response.ToggleApplicantResponse;
 
 public interface ClubService {
 
     void postClub(PostClubRequest request);
+    ToggleApplicantResponse toggleApplicant(ToggleApplicantRequest request);
 }
