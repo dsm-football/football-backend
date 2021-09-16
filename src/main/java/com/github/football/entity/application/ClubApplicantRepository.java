46b777d5ff33d78f19a9d218b004eaea55346d56
sonchaegeon <thscorjs1002@naver.com>
2021-09-16 22:51:02 +0900

    ⚡️ :: (#31) club applicant repository

---
+++ src/main/java/com/github/football/entity/application/ClubApplicantRepository.java
@@ -0,0 +1,6 @@
+package com.github.football.entity.application;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClubApplicantRepository extends CrudRepository<ClubApplicant, Long> {
+}
