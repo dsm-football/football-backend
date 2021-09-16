4790add634a9eeaf01de1cf178a415f545b55409
sonchaegeon <thscorjs1002@naver.com>
2021-09-16 18:04:07 +0900

    ⚰️ :: (#44) club_executive entity 삭제

--- src/main/java/com/github/football/entity/application/Application.java
+++ src/main/java/com/github/football/entity/application/Application.java
@@ -1,13 +1,11 @@
 package com.github.football.entity.application;
 
 import com.github.football.entity.application.embedded.ApplicationId;
-import com.github.football.entity.club.embedded.ClubExecutiveId;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
