4cc756a61016b0d169462a7c5bba64b41caf308c
sonchaegeon <thscorjs1002@naver.com>
2021-09-17 11:42:13 +0900

    ⚡️ :: (#49) get /club/applicant service 추상화

--- src/main/java/com/github/football/service/club/ClubService.java
+++ src/main/java/com/github/football/service/club/ClubService.java
@@ -2,10 +2,12 @@ package com.github.football.service.club;
 
 import com.github.football.dto.club.request.PostClubRequest;
 import com.github.football.dto.club.request.ToggleApplicantRequest;
+import com.github.football.dto.club.response.GetClubApplicantResponse;
 import com.github.football.dto.club.response.ToggleApplicantResponse;
 
 public interface ClubService {
 
     void postClub(PostClubRequest request);
     ToggleApplicantResponse toggleApplicant(ToggleApplicantRequest request);
+    GetClubApplicantResponse getClubApplicant();
 }
