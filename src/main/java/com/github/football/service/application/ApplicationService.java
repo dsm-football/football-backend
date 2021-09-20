c15aebbe7e732ad058947290c785a30de80dd2c6
sonchaegeon <thscorjs1002@naver.com>
2021-09-20 12:50:26 +0900

    ⚡️ :: (#52) get /application service 추상화

--- src/main/java/com/github/football/service/application/ApplicationService.java
+++ src/main/java/com/github/football/service/application/ApplicationService.java
@@ -1,8 +1,12 @@
 package com.github.football.service.application;
 
 import com.github.football.dto.application.request.PostApplicationRequest;
+import com.github.football.dto.application.response.GetApplicationResponse;
+
+import java.util.List;
 
 public interface ApplicationService {
 
     void postApplication(PostApplicationRequest request);
+    List<GetApplicationResponse> getApplication();
 }
