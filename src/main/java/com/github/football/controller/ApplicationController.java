7dee3973e27cf086c2212632d9342a2b4d4ea346
sonchaegeon <thscorjs1002@naver.com>
2021-09-20 12:51:29 +0900

    ⚡️ :: (#52) get /application controller

--- src/main/java/com/github/football/controller/ApplicationController.java
+++ src/main/java/com/github/football/controller/ApplicationController.java
@@ -1,10 +1,14 @@
 package com.github.football.controller;
 
 import com.github.football.dto.application.request.PostApplicationRequest;
+import com.github.football.dto.application.response.GetApplicationResponse;
 import com.github.football.service.application.ApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("application")
@@ -15,7 +19,12 @@ public class ApplicationController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void postApplicant(@RequestBody PostApplicationRequest request) {
+    public void postApplication(@RequestBody PostApplicationRequest request) {
         applicationService.postApplication(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetApplicationResponse>> getApplication() {
+        return new ResponseEntity<>(applicationService.getApplication(), HttpStatus.OK);
     }
 }
