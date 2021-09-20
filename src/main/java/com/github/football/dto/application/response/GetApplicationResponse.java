3888a238b2e2cd5c44d57eec18575c778a64e30b
sonchaegeon <thscorjs1002@naver.com>
2021-09-20 12:51:16 +0900

    ⚡️ :: (#52) get applicant response

---
+++ src/main/java/com/github/football/dto/application/response/GetApplicationResponse.java
@@ -0,0 +1,15 @@
+package com.github.football.dto.application.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GetApplicationResponse {
+
+    public String name;
+    public String area;
+    public Integer age;
+    public String gender;
+    public String profile;
+}
