59e62edad2590733045a3ab84115c378d00c1963
sonchaegeon <thscorjs1002@naver.com>
2021-09-20 12:51:05 +0900

    ⚡️ :: (#52) findByApplicationId_ClubApplicant method 추가

--- src/main/java/com/github/football/entity/application/ApplicationRepository.java
+++ src/main/java/com/github/football/entity/application/ApplicationRepository.java
@@ -1,9 +1,12 @@
 package com.github.football.entity.application;
 
 import com.github.football.entity.application.embedded.ApplicationId;
+import com.github.football.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ApplicationRepository extends CrudRepository<Application, ApplicationId> {
 
-
+    List<Application> findByApplicationId_ClubApplicant(ClubApplicant clubApplicant);
 }
