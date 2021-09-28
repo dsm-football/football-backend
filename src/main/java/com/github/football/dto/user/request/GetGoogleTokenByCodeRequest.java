93237108ad232ffa18c0265e75499349025f0ba0
sonchaegeon <thscorjs1002@naver.com>
2021-09-28 15:55:33 +0900

    ⚡️ :: (#63) get google token by code request

---
+++ src/main/java/com/github/football/dto/user/request/GetGoogleTokenByCodeRequest.java
@@ -0,0 +1,16 @@
+package com.github.football.dto.user.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GetGoogleTokenByCodeRequest {
+
+    private String code;
+    private String name;
+    private Integer age;
+    private Integer positionId;
+    private Integer genderId;
+    private String area;
+}
