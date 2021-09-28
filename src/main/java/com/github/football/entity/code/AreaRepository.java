085eae3e56c0af519925505709a5b9e6aa539d11
sonchaegeon <thscorjs1002@naver.com>
2021-09-28 15:56:28 +0900

    ⚡️ :: (#63) find by name method

--- src/main/java/com/github/football/entity/code/AreaRepository.java
+++ src/main/java/com/github/football/entity/code/AreaRepository.java
@@ -2,5 +2,9 @@ package com.github.football.entity.code;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AreaRepository extends CrudRepository<Area, Integer> {
+
+    Optional<Area> findByName(String name);
 }
