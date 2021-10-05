7adcaf7fd496a5db0c51b8d4e0314481fe3ee83f
sonchaegeon <thscorjs1002@naver.com>
2021-10-05 19:31:07 +0900

    ⚡️ :: (#58) game option repository

---
+++ src/main/java/com/github/football/entity/game/GameOptionRepository.java
@@ -0,0 +1,6 @@
+package com.github.football.entity.game;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GameOptionRepository extends CrudRepository<GameOption, Long> {
+}
