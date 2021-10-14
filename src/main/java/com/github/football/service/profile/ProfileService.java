454ac9e9135a718c9ee00e33b705a65def1ff6c8
sonchaegeon <thscorjs1002@naver.com>
2021-10-14 12:09:28 +0900

    ⚡️ :: (#70) patch /profile service 추상화

--- src/main/java/com/github/football/service/profile/ProfileService.java
+++ src/main/java/com/github/football/service/profile/ProfileService.java
@@ -1,7 +1,9 @@
 package com.github.football.service.profile;
 
+import com.github.football.dto.profile.request.UpdateProfileRequest;
 import com.github.football.dto.profile.response.GetProfileResponse;
 
 public interface ProfileService {
     GetProfileResponse getProfile(Long id);
+    void updateProfile(UpdateProfileRequest request);
 }
