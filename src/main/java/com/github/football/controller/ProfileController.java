57aed1753bc9caed7ede82cf74890450f6f0f418
sonchaegeon <thscorjs1002@naver.com>
2021-10-14 12:10:19 +0900

    ⚡️ :: (#70) patch /profile controller

--- src/main/java/com/github/football/controller/ProfileController.java
+++ src/main/java/com/github/football/controller/ProfileController.java
@@ -1,15 +1,13 @@
 package com.github.football.controller;
 
 import com.github.football.dto.club.response.GetClubResponse;
+import com.github.football.dto.profile.request.UpdateProfileRequest;
 import com.github.football.dto.profile.response.GetProfileResponse;
 import com.github.football.service.profile.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("profile")
@@ -21,5 +19,11 @@ public class ProfileController {
     @GetMapping("{id}")
     public ResponseEntity<GetProfileResponse> getProfile(@PathVariable("id") Long id) {
         return new ResponseEntity<>(profileService.getProfile(id), HttpStatus.OK);
+    }
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProfile(@RequestBody UpdateProfileRequest request) {
+        profileService.updateProfile(request);
     }
 }
