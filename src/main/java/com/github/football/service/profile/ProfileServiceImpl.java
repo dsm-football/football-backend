af49a403ed12b8679023ea77e91c2654cdff32e4
sonchaegeon <thscorjs1002@naver.com>
2021-10-14 12:09:16 +0900

    ⚡️ :: (#70) patch /profile service 구현체

--- src/main/java/com/github/football/service/profile/ProfileServiceImpl.java
+++ src/main/java/com/github/football/service/profile/ProfileServiceImpl.java
@@ -1,15 +1,23 @@
 package com.github.football.service.profile;
 
+import com.github.football.dto.profile.request.UpdateProfileRequest;
 import com.github.football.dto.profile.response.GetProfileResponse;
 import com.github.football.entity.club.Club;
 import com.github.football.entity.code.Area;
+import com.github.football.entity.code.AreaRepository;
 import com.github.football.entity.code.Position;
+import com.github.football.entity.code.PositionRepository;
 import com.github.football.entity.user.User;
 import com.github.football.entity.user.UserRepository;
+import com.github.football.exception.type.AreaNotFoundException;
+import com.github.football.exception.type.CredentialsNotFoundException;
+import com.github.football.exception.type.PositionNotFoundException;
 import com.github.football.exception.type.UserNotFoundException;
+import com.github.football.security.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
@@ -17,6 +25,8 @@ import java.util.Optional;
 public class ProfileServiceImpl implements ProfileService {
 
     private final UserRepository userRepository;
+    private final PositionRepository positionRepository;
+    private final AreaRepository areaRepository;
 
     @Override
     public GetProfileResponse getProfile(Long id) {
@@ -44,5 +54,22 @@ public class ProfileServiceImpl implements ProfileService {
                 .profile(user.getProfile())
                 .bio(user.getBio())
                 .build();
+    }
+
+    @Override
+    @Transactional
+    public void updateProfile(UpdateProfileRequest request) {
+        User user = userRepository.findByEmail(UserFacade.getEmail())
+                .orElseThrow(CredentialsNotFoundException::new);
+
+        Position position = positionRepository.findById(request.getPositionId())
+                        .orElseThrow(PositionNotFoundException::new);
+
+        Area area = areaRepository.findByName(request.getArea()).orElseGet(() -> areaRepository.save(
+                Area.builder()
+                        .name(request.getArea())
+                        .build()
+        ));
+        user.updateProfile(request.getAge(), position, area, request.getBio());
     }
 }
