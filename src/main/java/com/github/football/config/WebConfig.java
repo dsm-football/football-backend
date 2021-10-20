f37a021bcf7b765fb989ee266e24bf8095241a61
sonchaegeon <thscorjs1002@naver.com>
2021-10-20 10:28:37 +0900

    ⚙️ :: cors 허용
    개발기간에만 ㅎㅎ

--- src/main/java/com/github/football/config/WebConfig.java
+++ src/main/java/com/github/football/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false)
+                .allowedHeaders("*")
                 .allowedMethods("*");
     }
 }
