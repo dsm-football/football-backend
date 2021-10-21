c1794e864f8303c07a6b30d740c2408a4410ff18
sonchaegeon <thscorjs1002@naver.com>
2021-10-21 12:09:30 +0900

    ⚡️ :: (#75) kick member request

---
+++ src/main/java/com/github/football/dto/club/request/KickMemberRequest.java
@@ -0,0 +1,10 @@
+package com.github.football.dto.club.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KickMemberRequest {
+    private Long userId;
+}
