15f9ae06c86c0184f0b8b628947aadd4de65b9fe
sonchaegeon <thscorjs1002@naver.com>
2021-10-27 16:45:41 +0900

    ⚡️ :: (#87) check club name request

---
+++ src/main/java/com/github/football/dto/club/request/CheckClubNameRequest.java
@@ -0,0 +1,12 @@
+package com.github.football.dto.club.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CheckClubNameRequest {
+    private String name;
+}
