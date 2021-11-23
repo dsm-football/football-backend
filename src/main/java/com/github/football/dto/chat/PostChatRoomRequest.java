5da1d5d2372451f50dba5f40c7a8e0cee08cfb87
sonchaegeon <thscorjs1002@naver.com>
2021-11-24 00:13:23 +0900

    ⚡️ :: (#104) post chat room request

---
+++ src/main/java/com/github/football/dto/chat/PostChatRoomRequest.java
@@ -0,0 +1,10 @@
+package com.github.football.dto.chat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostChatRoomRequest {
+    private Long userId;
+}
