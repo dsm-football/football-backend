a8a0613a0f3bf291179dfa8e35fc292d950579d3
sonchaegeon <thscorjs1002@naver.com>
2021-11-25 22:59:00 +0900

    ⚡️ :: (#108) get /chat/history interface 추상화

--- src/main/java/com/github/football/service/chat/ChatService.java
+++ src/main/java/com/github/football/service/chat/ChatService.java
@@ -1,8 +1,12 @@
 package com.github.football.service.chat;
 
-import com.github.football.dto.chat.PostChatRoomRequest;
+import com.github.football.dto.chat.request.PostChatRoomRequest;
+import com.github.football.dto.chat.response.GetChatHistoryResponse;
+
+import java.util.List;
 
 public interface ChatService {
 
     void postChatRoom(PostChatRoomRequest request);
+    List<GetChatHistoryResponse> getChatHistory();
 }
