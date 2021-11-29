14f593bfa1653fddea7fdd37ecafc937d86a2184
sonchaegeon <thscorjs1002@naver.com>
2021-11-29 17:17:20 +0900

    ♻️ :: (#110) file name changed

--- src/main/java/com/github/football/dto/user/request/RegisterRequest.java
+++ src/main/java/com/github/football/dto/user/request/RegisterRequest.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class GetGoogleTokenByCodeRequest {
+public class RegisterRequest {
 
     private String code;
     private String name;
