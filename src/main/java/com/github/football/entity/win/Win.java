5902fb12c8e5cb8d891e2d5a6cf8046eedca1401
Seung jun <poodle2405@gmail.com>
2021-11-30 11:28:50 +0900

    win entity
    create win entity

---
+++ src/main/java/com/github/football/entity/win/Win.java
@@ -0,0 +1,38 @@
+package com.github.football.entity.win;
+
+import com.github.football.entity.club.Club;
+import com.github.football.entity.game.Game;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@Entity
+public class Win {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Boolean gameResult;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "win_club_id")
+    private Club winClubId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lose_club_id")
+    private Club loseClubId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    public Win(Boolean gameResult){
+        this.gameResult = gameResult;
+    }
+}
