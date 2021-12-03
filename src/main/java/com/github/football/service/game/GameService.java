60bb867464b3b01436809fbfab2eadfa6ca99147
sonchaegeon <thscorjs1002@naver.com>
2021-12-03 10:17:18 +0900

    ⚡️ :: (#114) post /game/list service 추상화

--- src/main/java/com/github/football/service/game/GameService.java
+++ src/main/java/com/github/football/service/game/GameService.java
@@ -2,8 +2,10 @@ package com.github.football.service.game;
 
 import com.github.football.dto.game.request.PostGameApplicationRequest;
 import com.github.football.dto.game.request.PostGameRequest;
+import com.github.football.dto.game.request.PostResultRequest;
 
 public interface GameService {
     void postGame(PostGameRequest request);
     void postGameApplication(PostGameApplicationRequest request);
+    void postResult(PostResultRequest request);
 }
