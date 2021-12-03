b17aba19f5b236e37ae8a0b7cb5ff999e5f97112
sonchaegeon <thscorjs1002@naver.com>
2021-12-03 10:03:21 +0900

    ⚡️ :: (#114) post /game/result controller

--- src/main/java/com/github/football/controller/GameController.java
+++ src/main/java/com/github/football/controller/GameController.java
@@ -1,8 +1,8 @@
 package com.github.football.controller;
 
-import com.github.football.dto.club.request.PostClubRequest;
 import com.github.football.dto.game.request.PostGameApplicationRequest;
 import com.github.football.dto.game.request.PostGameRequest;
+import com.github.football.dto.game.request.PostResultRequest;
 import com.github.football.service.game.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,5 +25,11 @@ public class GameController {
     @ResponseStatus(HttpStatus.CREATED)
     public void postGameApplication(@RequestBody PostGameApplicationRequest request) {
         gameService.postGameApplication(request);
+    }
+
+    @PostMapping("result")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postResult(@RequestBody PostResultRequest request) {
+        gameService.postResult(request);
     }
 }
